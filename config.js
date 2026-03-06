deb3ba3a30ac3787d7bd289497c7bec2feef7a3b
khaledxal <165736235+khaledxal@users.noreply.github.com>
2026-03-06 13:51:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// ⛔ هذا الملف لا يُرفع على GitHub - تأكد أنه في .gitignore
+// ⛔ هذا الملف لا يُرفع على GitHub - تأكد أنه في .gitignoreD
 window.GEMINI_KEY = "AIzaSyAa6xt1_rZIOUYq0ezUI_CSk8pOqdRuKXI";
