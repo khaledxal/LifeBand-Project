f80762f462d46795477138d97ce70478b5434b57
khaledxal <165736235+khaledxal@users.noreply.github.com>
2026-03-06 13:39:06 +0300

    س

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // ⛔ هذا الملف لا يُرفع على GitHub - تأكد أنه في .gitignore
-window.GEMINI_KEY = "AIzaSyC_2oDtObq9iqj2fPxjvU7AoteaPMhpqX8";
+window.GEMINI_KEY = "AIzaSyAa6xt1_rZIOUYq0ezUI_CSk8pOqdRuKXI";
